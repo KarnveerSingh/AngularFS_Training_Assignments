d7319c111a218f51fab8fb411b060d1bd079a47d
KarnveerSingh <104125153+KarnveerSingh@users.noreply.github.com>
2022-04-27 18:04:59 +0530

    Add files via upload

---
+++ 02.Angular/Day1/Task4/product.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+  productname:string = '';
+  unitprice:number = 0;
+  quantity:number = 1;
+  totalAmount:number = 0;
+  mystyle:string = '';
+
+   Button_Click(){
+    this.totalAmount = this.unitprice*this.quantity;
+
+    if(this.totalAmount>10000){
+      this.mystyle = "color:green;"
+    }
+  }
+
+}
