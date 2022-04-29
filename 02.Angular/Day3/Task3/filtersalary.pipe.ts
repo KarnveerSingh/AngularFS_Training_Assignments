01dc08e4cb4f9bf1fc63cd44e50e093ebe42d373
KarnveerSingh <104125153+KarnveerSingh@users.noreply.github.com>
2022-04-29 16:42:42 +0530

    Update filtersalary.pipe.ts

--- 02.Angular/Day3/Task3/filtersalary.pipe.ts
+++ 02.Angular/Day3/Task3/filtersalary.pipe.ts
@@ -7,7 +7,7 @@ export class FiltersalaryPipe implements PipeTransform {
 
   transform(inputData:any[],  value1:string, value2:string): any[]
   {
-    console.log("hi");
+   
     return inputData.filter(item => item["empsal"] >= value1 && item['empsal'] <= value2);
   }
 
