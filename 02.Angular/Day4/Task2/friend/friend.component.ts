0ce9e580bbbd400347b1dd07c8950a6e85049677
KarnveerSingh <104125153+KarnveerSingh@users.noreply.github.com>
2022-05-02 19:34:19 +0530

    Add files via upload

---
+++ 02.Angular/Day4/Task2/friend/friend.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friend',
+  templateUrl: './friend.component.html',
+  styleUrls: ['./friend.component.css']
+})
+export class FriendComponent  {
+
+ 
+  friendArr:string[] = ["Scott", "Smith", "Sandy", "Sam"];
+}
---
+++ 02.Angular/Day4/Task2/product-detail/product-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Products } from '../Product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent {
+
+  productArr: Products[] = [
+    { pid: 1, pname: "product1", price: 100, category: "category 1", qty: 4 },
+    { pid: 2, pname: "product2", price: 200, category: "category 3", qty: 2 },
+    { pid: 3, pname: "product3", price: 300, category: "category 1", qty: 4 },
+    { pid: 4, pname: "product4", price: 400, category: "category 7", qty: 6 }
+  ];
+}
