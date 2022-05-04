c47796e2d629e9dcddd9aa3516db403cf788ecc3
KarnveerSingh <104125153+KarnveerSingh@users.noreply.github.com>
2022-05-04 17:04:56 +0530

    Update todo-task.component.ts

--- 02.Angular/Day5/Task1/todo-task/todo-task.component.ts
+++ 02.Angular/Day5/Task1/todo-task/todo-task.component.ts
@@ -7,15 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoTaskComponent implements OnInit {
 
-  inputItem:string = '';
+  inputItem: string = '';
+  id:number=0;
 
-  todoListObj:any[] =[
-    {id:1,itemName:"learn react js basic",isDone:true},
-    {id:2,itemName:"prectice react js",isDone:false},
-    {id:3,itemName:"learn redux",isDone:false},
-    {id:4,itemName:"code portfolio in react",isDone:false},
-    {id:5,itemName:"learn react native",isDone:false}
+  todoListObj: any[] = [
+    { id: 1, itemName: "learn react js basic", isDone: true },
+    { id: 2, itemName: "prectice react js", isDone: false },
+    { id: 3, itemName: "learn redux", isDone: false },
+    { id: 4, itemName: "code portfolio in react", isDone: false },
+    { id: 5, itemName: "learn react native", isDone: false }
   ];
+
 
   constructor() { }
 
@@ -24,35 +26,50 @@ export class TodoTaskComponent implements OnInit {
 
 
 
-  addNew_click(){
-    let todoObj:any = new Object();
+  addNew_click() {
+    let todoObj: any = new Object();
     todoObj.itemName = this.inputItem;
     this.todoListObj.push(todoObj);
   }
 
 
-  delete_click(id:number):void {
+  delete_click(id: number): void {
     let index = this.todoListObj.findIndex(item => item.id == id);
     this.todoListObj.splice(index, 1);
   }
 
 
 
-  update_click(){
-    
+
+  select_click(id: number) {
+    let obj: any = this.todoListObj.filter(item => item.id == id);
+    this.inputItem = obj[0].itemName;
+    // obj.itemName = this.inputItem; 
+    this.id = id;
+
   }
 
 
-  DeleteDone_click(){
+  update_click() {
+    let obj: any = this.todoListObj.filter(item => item.id == this.id);
+   
+    obj[0].itemName = this.inputItem; 
+    obj[0].id = this.id; 
+   
+  
+
+  }
+
+
+  DeleteDone_click() {
     this.todoListObj = this.todoListObj.filter(todo => todo.isDone == false);
-    
+
   }
 
-  DeleteAll_click(){
+  DeleteAll_click() {
     this.todoListObj = [{}];
   }
 
 
-  
-  
+
 }
