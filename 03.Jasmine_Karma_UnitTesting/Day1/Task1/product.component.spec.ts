17bb28a7585efe36bd772f4764289f2d4889d43e
KarnveerSingh <104125153+KarnveerSingh@users.noreply.github.com>
2022-05-10 18:37:42 +0530

    Update product.component.spec.ts

--- 03.Jasmine_Karma_UnitTesting/Day1/Task1/product.component.spec.ts
+++ 03.Jasmine_Karma_UnitTesting/Day1/Task1/product.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { ProductComponent } from './product.component';
 
@@ -8,7 +9,8 @@ describe('ProductComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ProductComponent]
+      declarations: [ProductComponent],
+      imports: [FormsModule]
     })
       .compileComponents();
   });
@@ -44,7 +46,7 @@ describe('ProductComponent', () => {
     component.unitprice = 55;
     component.quantity = 2;
     component.getTotalAmount();
-    const strOutput:string  = component.totalAmount;
+    const strOutput: string = component.totalAmount;
     expect(strOutput).toBe("Total Amount : 110");
   });
 
@@ -54,10 +56,58 @@ describe('ProductComponent', () => {
     component.unitprice = component.unitprice;
     component.quantity = component.quantity;
     component.getTotalAmount();
-    const strOutput:string  = component.totalAmount;
+    const strOutput: string = component.totalAmount;
     expect(strOutput).toBe("Total Amount : 0");
   });
 
-  
+
+  //check Quantity is updating
+  it('should set the Quantity to 4 in textbox', () => {
+
+    let inputArray = fixture.nativeElement.querySelectorAll('input');
+    inputArray[2].value = 4;
+    inputArray[2].dispatchEvent(new Event('input'));
+    expect(component.quantity).toBe(4);
+  });
+
+
+
+  //check unit price is updating
+  it('should set the Unit price to 6 in textbox', () => {
+
+    let inputArray = fixture.nativeElement.querySelectorAll('input');
+
+    inputArray[1].value = 6;
+    inputArray[1].dispatchEvent(new Event('input'));
+    expect(component.unitprice).toBe(6);
+  });
+
+
+
+  it('should display message in span "Total Amount : 24"', () => {
+
+    const inputArray = fixture.nativeElement.querySelectorAll("input");
+
+    inputArray[0].value = 'product 1';
+    inputArray[0].dispatchEvent(new Event('input'));
+
+    inputArray[1].value = 12;
+    inputArray[1].dispatchEvent(new Event('input'));
+
+
+    inputArray[2].value = 2;
+    inputArray[2].dispatchEvent(new Event('input'));
+
+    let inputObj = fixture.nativeElement.querySelector('button');
+    inputObj.dispatchEvent(new Event('click'));
+
+    fixture.detectChanges();
+
+    let parObj = fixture.nativeElement.querySelector('span');
+    expect(parObj.textContent).toBe("Total Amount : 24");
+
+
+  });
 
 });
+
